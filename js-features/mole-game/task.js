d550febfa39136518a7eff59f2749157d2600233
Ekaterina Karelina <kiaristova@gmail.com>
2024-02-04 23:15:54 +0300

    Задание 1.3 правка отступов

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,21 +3,21 @@ const dead = document.getElementById('dead');
 const lost = document.getElementById('lost');
 
 elementsList.forEach(element => {
-    element.onclick = function() {
-        if (element.classList.contains('hole_has-mole')) {
-            dead.textContent = String(Number(dead.textContent) + 1);
-        } else {
-            lost.textContent = String(Number(lost.textContent) + 1);
-        }
-        if ((Number(dead.textContent) > 9) && (Number(lost.textContent) < 5)) {
-            alert("Победа!");
-            dead.textContent = "0";
-            lost.textContent = "0";
-        }
-        if (Number(lost.textContent) > 4) {
-            alert("Вы проиграли!");
-            dead.textContent = "0";
-            lost.textContent = "0";
-        }
-    }
+	element.onclick = function() {
+		if (element.classList.contains('hole_has-mole')) {
+			dead.textContent = String(Number(dead.textContent) + 1);
+		} else {
+			lost.textContent = String(Number(lost.textContent) + 1);
+		}
+		if ((Number(dead.textContent) > 9) && (Number(lost.textContent) < 5)) {
+			alert("Победа!");
+			dead.textContent = "0";
+			lost.textContent = "0";
+		}
+		if (Number(lost.textContent) > 4) {
+			alert("Вы проиграли!");
+			dead.textContent = "0";
+			lost.textContent = "0";
+		}
+	}
 });
