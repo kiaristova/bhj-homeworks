4b2effcc5ea3ce00f0fd93034b02f6dc0c8ae989
Ekaterina Karelina <kiaristova@gmail.com>
2024-03-28 20:54:02 +0300

    first commit tabs

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,12 +1,11 @@
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.tab__content');
 
-// Пишем логику по добавлению звезд
 tabs.forEach((tab, index) => {
-    tab.addEventListener('click', () => {
-        tabs.forEach(s => s.classList.remove('tab_active'));
-        contents.forEach((k) => k.classList.remove('tab__content_active'));
-        contents[index].classList.add('tab__content_active');
-        tab.classList.add('tab_active');
-});
+	tab.addEventListener('click', () => {
+		tabs.forEach(s => s.classList.remove('tab_active'));
+		contents.forEach((k) => k.classList.remove('tab__content_active'));
+		contents[index].classList.add('tab__content_active');
+		tab.classList.add('tab_active');
+	});
 })
