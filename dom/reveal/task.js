9e0d5ecbb9c7e0be0c2310c1ee24107f133a9aa8
Ekaterina Karelina <kiaristova@gmail.com>
2024-04-07 16:40:12 +0300

    reveal: fix formatting and last condition

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,25 +1,27 @@
 const revealList = document.querySelectorAll('.reveal')
 
 function isVisible(el) {
-    const { top, bottom } = el.getBoundingClientRect()
+	const {
+		top,
+		bottom
+	} = el.getBoundingClientRect()
 
-    if (bottom < 0) {
-        return false
-    }
+	if (bottom < 0) {
+		return false
+	}
 
-    if (top > window.innerHeight) {
-        return false
-    }
-    return true
+	if (top > window.innerHeight) {
+		return false
+	}
+	return true
 }
 
 document.addEventListener('scroll', (event) => {
-    revealList.forEach((el, index) => {
-        if (isVisible(el)) {
-            el.classList.add('reveal_active');
-        }
-        if (!isVisible(el)) {
-            el.classList.remove('reveal_active');
-        }
-    })
+	revealList.forEach((el, index) => {
+		if (isVisible(el)) {
+			el.classList.add('reveal_active');
+		} else {
+			el.classList.remove('reveal_active');
+		}
+	})
 })
